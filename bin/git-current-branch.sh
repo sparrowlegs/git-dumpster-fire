71941e332030146d731a32bacf072fe17e3f2dd2
JC Mann <mr.sparrowlegs@gmail.com>
2018-12-06 10:20:58 -0500

    Initial check-in

---
+++ bin/git-current-branch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Yup, this is the "current" way to get the current branch name.
+# Returns nothing if the current directory is not a local git repo.
+
+git symbolic-ref --short HEAD 2>/dev/null
---
+++ bin/git-local-only-branches.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Return a list of branches which do not have a remote (upstream) branch.
+# Return nothing if current directory is not a local git repo.
+
+git_info=`git show --oneline HEAD 2>/dev/null`
+
+if [ -z "${git_info}" ]; then
+	exit 1
+fi
+
+comm -23 <(git branch | sed 's/\* /  /' | sort) <(git branch -r | sed 's|origin/||' | sort) | perl -pe 's/^ +//'
